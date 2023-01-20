a8759db19ff0f7e681c45e43d4f18cec899c4fac
jbrazjr.es <jbrazjr.es@gmail.com>
2023-01-20 18:51:31 -0300

    chore: List Haircut

---
+++ backend/src/controllers/haircut/ListHaircutController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { ListHaircutService } from "../../services/haircut/ListHaircutService";
+
+class ListHaircutController {
+  async handle( request: Request, response: Response ) {
+    const user_id = request.user_id
+    const status = request.query.status as string
+
+    const listHaircutService = new ListHaircutService()
+
+    const haircuts = await listHaircutService.execute({ user_id, status })
+
+    return response.json(haircuts)
+  }
+}
+
+export { ListHaircutController }
