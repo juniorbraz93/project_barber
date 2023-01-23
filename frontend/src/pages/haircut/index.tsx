22fdc74b74481acd1bde05fb058bab5f17d39970
jbrazjr.es <jbrazjr.es@gmail.com>
2023-01-22 22:06:58 -0300

    style: page  haircuts

---
+++ frontend/src/pages/haircut/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+import { Sidebar } from "@/components/sidebar";
+
+export default function NewHaircut(){
+  return (
+    <>
+      <Head>
+        <title>BarbePro - Cadastrar novo corte </title>
+      </Head>
+    </>    
+  )
+}
